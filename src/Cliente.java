a59cf10d7955c09f15d2b0573246a80a52635888
HarounDominique <haroundominique@gmail.com>
2023-05-03 21:45:31 +0200

    Initial commit

---
+++ src/Cliente.java
@@ -0,0 +1,86 @@
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+public class Cliente {
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+
+        InetAddress destino = InetAddress.getLocalHost();
+        int port = 1234; //puerto al que enviar el mensaje, el del servidor
+        DatagramSocket socket = new DatagramSocket(34567); //Puerto local, el del cliente
+
+        do {
+            //contenedor del mensaje más o menos
+            byte[] mensaje = new byte[1024];
+
+            //mensaje, digamos, en bruto
+            Persona p1 = new Persona("pepe", 67);
+            System.out.printf("Enviado objeto persona");
+
+            ObjectOutputStream oos = new ObjectOutputStream(p1);
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            mensaje = p1.getBytes();
+
+            //Datagram a ENVIAR:
+            DatagramPacket envio = new DatagramPacket(mensaje, mensaje.length, destino, port);
+
+
+            //Envío de datagram:
+            socket.send(envio);
+
+            byte[] bufer = new byte[1024]; //bufer que recibirá el datagram del cliente
+            //esperando al datagram:
+            System.out.println("Esperando al datagrama...");
+            DatagramPacket paqueteRecibir = new DatagramPacket(bufer, bufer.length);
+            socket.receive(paqueteRecibir); //recibiendo datagrama
+            System.out.println("Recibido datagrama:");
+            String mensajeRecibido = new String(paqueteRecibir.getData(), 0, paqueteRecibir.getLength(), StandardCharsets.UTF_8);
+            System.out.println(mensajeRecibido);
+
+        } while (!texto.equals("*"));
+        socket.close();
+    }
+
+    public static class Persona {
+        private String nombre;
+        private int edad;
+
+        public Persona(String nombre, int edad) {
+            this.nombre = nombre;
+            this.edad = edad;
+        }
+
+        public String getNombre() {
+            return nombre;
+        }
+
+        public void setNombre(String nombre) {
+            this.nombre = nombre;
+        }
+
+        public int getEdad() {
+            return edad;
+        }
+
+        public void setEdad(int edad) {
+            this.edad = edad;
+        }
+
+        @Override
+        public String toString() {
+            return "Persona{" +
+                    "nombre='" + nombre + '\'' +
+                    ", edad=" + edad +
+                    '}';
+        }
+    }
+}
+
+
